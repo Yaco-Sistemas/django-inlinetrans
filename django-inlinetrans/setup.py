062973bd4a6b0adcfe0792f9261457637b9bd2e2
ant30tx <ant30tx@e9feeca8-1875-11de-babb-ddc7aea8d8a1>
2010-12-22 17:39:29 +0000

    Remove zip_safe to be compatible with merengue
    git-svn-id: http://django-inlinetrans.googlecode.com/svn/trunk@27 e9feeca8-1875-11de-babb-ddc7aea8d8a1

--- django-inlinetrans/setup.py
+++ django-inlinetrans/setup.py
@@ -41,7 +41,6 @@ setup(name='django-inlinetrans',
       license='LGPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
-      zip_safe=True,
       install_requires=[
           # -*- Extra requirements: -*-
       ],
