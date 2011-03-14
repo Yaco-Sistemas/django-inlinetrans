acdf9b104d51a65c5ae66562ff4753e8fbbe0a72
msaelices <msaelices@e9feeca8-1875-11de-babb-ddc7aea8d8a1>
2011-03-14 22:35:40 +0000

    Preparing new version.
    git-svn-id: http://django-inlinetrans.googlecode.com/svn/trunk@52 e9feeca8-1875-11de-babb-ddc7aea8d8a1

--- django-inlinetrans/setup.py
+++ django-inlinetrans/setup.py
@@ -4,7 +4,7 @@ import sys, os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.4.2'
+version = '0.4.3'
 
 long_description = (
     read('README.txt')
