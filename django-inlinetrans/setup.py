55b1296cab1cf09b0201cf112f8ac12db89fe9a4
msaelices <msaelices@e9feeca8-1875-11de-babb-ddc7aea8d8a1>
2011-03-15 23:41:59 +0000

    Changing the encoding.
    git-svn-id: http://django-inlinetrans.googlecode.com/svn/trunk@53 e9feeca8-1875-11de-babb-ddc7aea8d8a1

--- django-inlinetrans/setup.py
+++ django-inlinetrans/setup.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 from setuptools import setup, find_packages
 import sys, os
 
@@ -23,14 +25,13 @@ long_description = (
     '********\n')
 
 
-
 setup(name='django-inlinetrans',
       version=version,
       description="Is a django application that allows the translation of django templtes from the rendered html in the browser",
       long_description=long_description,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='django translation',
-      author='Antonio P\xc3\xa9rez-Aranda Alcaide',
+      author='Antonio Pérez-Aranda Alcaide',
       author_email='ant30tx@gmail.com',
       url='http://code.google.com/p/django-inlinetrans/',
       license='LGPL',
